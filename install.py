2869e9a48cfb04d71aa17644c410c0ff77a4e617
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2025-12-10 15:35:52 +0100

    fix

--- install.py
+++ install.py
@@ -381,7 +381,7 @@ if __name__ == "__main__":
             )
         )
     except ImportError:
-        handler = logging.StreamHandler(sys.stdout)
+        handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter(fmt, datefmt=datefmt))
     logger.addHandler(handler)
     logger.setLevel(getattr(logging, args.log_level.upper(), logging.INFO))
